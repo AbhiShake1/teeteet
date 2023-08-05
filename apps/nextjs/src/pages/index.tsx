b9ec95ec40b2c03160abeb1174a304bbc2237a17
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-06 01:07:48 +0545

    Revert "[WIP] test coords"
    This reverts commit 928c9e4a39444642003c9dc2b83fdcd2fd1018cd.

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,17 +1,9 @@
 import type {NextPage} from "next";
 import Head from "next/head";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {CreateAccountForm} from "../components/forms/CreateAccount";
 
 const Home: NextPage = () => {
-    const [coords, setCoords] = useState<GeolocationCoordinates | null>(null)
-
-    useEffect(() => {
-        navigator.geolocation.watchPosition(({coords}) => {
-            setCoords(coords)
-        })
-        return () => navigator.geolocation.clearWatch(1)
-    }, [])
     return (
         <>
             <Head>
@@ -20,7 +12,6 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className='flex items-center justify-center'>
-                {coords?.latitude} {coords?.longitude}
                 <CreateAccountForm/>
             </main>
         </>
