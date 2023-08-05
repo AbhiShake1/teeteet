928c9e4a39444642003c9dc2b83fdcd2fd1018cd
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-05 21:42:51 +0545

    [WIP] test coords

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type {NextPage} from "next";
 import Head from "next/head";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {CreateAccountForm} from "../components/forms/CreateAccount";
 
 const Home: NextPage = () => {
+    const [coords, setCoords] = useState<GeolocationCoordinates | null>(null)
+
+    useEffect(() => {
+        navigator.geolocation.watchPosition(({coords}) => {
+            setCoords(coords)
+        })
+        return () => navigator.geolocation.clearWatch(1)
+    }, [])
     return (
         <>
             <Head>
@@ -12,6 +20,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className='flex items-center justify-center'>
+                {coords?.latitude} {coords?.longitude}
                 <CreateAccountForm/>
             </main>
         </>
