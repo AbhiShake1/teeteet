eac4464974a26e7e81d3ee3f0e7ce88c7b4eaa8a
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-06 01:07:45 +0545

    Revert "create new blank package"
    This reverts commit 588bd5c61f6ecba1e90332e25aaf6fea634b90fb.

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -9,7 +9,7 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ["@acme/api", "@acme/db", "@acme/components"],
+  transpilePackages: ["@acme/api", "@acme/db"],
   // We already do linting on GH actions
   eslint: {
     ignoreDuringBuilds: !!process.env.CI,
