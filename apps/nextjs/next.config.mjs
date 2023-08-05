9f961080a1a3d77b91e520da765b6f4e4d6fb86f
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-06 01:20:37 +0545

    add back blank components package

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -9,7 +9,7 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ["@acme/api", "@acme/db"],
+  transpilePackages: ["@acme/api", "@acme/db", "@acme/components"],
   // We already do linting on GH actions
   eslint: {
     ignoreDuringBuilds: !!process.env.CI,
