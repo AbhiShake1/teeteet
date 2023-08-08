f0c7ab725c16015ac4a61cc243dfd7c3cd814860
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-09 01:26:02 +0545

    change navmenu items

--- packages/components/src/core/TNavigationMenu.tsx
+++ packages/components/src/core/TNavigationMenu.tsx
@@ -17,44 +17,30 @@ export function TNavigationMenu() {
                     <NavigationMenuTrigger>BUY A CAR</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/"
-                                    >
-                                        {/*<Icons.logo className="h-6 w-6" />*/}
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            shadcn/ui
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Beautifully designed components built with Radix UI and
-                                            Tailwind CSS.
-                                        </p>
-                                    </a>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/docs" title="Introduction">
-                                Re-usable components built using Radix UI and Tailwind CSS.
+                            <ListItem href="/cars" title="Popular">
+                                Most browsed cars of all time
                             </ListItem>
-                            <ListItem href="/docs/installation" title="Installation">
-                                How to install dependencies and structure your app.
+                            <ListItem href="/cars" title="Top Rated">
+                                Best user feedbacks
                             </ListItem>
-                            <ListItem href="/docs/primitives/typography" title="Typography">
-                                Styles for headings, paragraphs, lists...etc
+                            <ListItem href="/cars" title="Latest">
+                                Latest arrivals
+                            </ListItem>
+                            <ListItem href="/cars" title="Our recommendations">
+                                Teeteet's recommendations
                             </ListItem>
                         </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <Link href="/docs" legacyBehavior passHref>
+                    <Link href="/cars" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                             SELL A CAR
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <Link href="/docs" legacyBehavior passHref>
+                    <Link href="/cars" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                             CAR FINANCE
                         </NavigationMenuLink>
