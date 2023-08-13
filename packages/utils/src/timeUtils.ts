048e8793e66739256db5b527949d35d00dc2dfd0
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-13 19:15:06 +0545

    add hours utility

--- packages/utils/src/timeUtils.ts
+++ packages/utils/src/timeUtils.ts
@@ -1,4 +1,11 @@
-export const days = (days: number) => 60 * 60 * 24 * days
+
+export const hours = (hours: number) => 60 * 60
+export const days = (days: number) => hours(1) * 24 * days
+
+// days
 export const oneDay = days(1)
 export const twoDays = days(2)
 export const threeDays = days(3)
+
+// hours
+export const oneHour = hours(1)
