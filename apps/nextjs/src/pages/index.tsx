dbc3cf18b63af0225cde2cf1d9baa509c5a3f528
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-14 19:55:03 +0545

    decrease x-padding in home page

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main
-                className='flex flex-col items-start px-32 py-16 bg-cover bg-no-repeat mix-blend-difference bg-[url(/bg.png)] h-[88vh]'>
+                className='flex flex-col items-start p-16 bg-cover bg-no-repeat mix-blend-difference bg-[url(/bg.png)] h-[88vh]'>
                 <div className='max-w-[40vw] space-y-4'>
                     <h3 className="scroll-m-20 text-4xl font-extrabold tracking-wide lg:text-5xl lg:leading-snug leading-snug">
                         Your ultimate destination to find affordable cars!
