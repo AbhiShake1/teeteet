54c4abf840ae5da7928794d205d9985e5082228f
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-15 19:15:56 +0545

    migrate cars finance page

--- apps/nextjs/src/app/cars/finance/page.tsx
+++ apps/nextjs/src/app/cars/finance/page.tsx
@@ -1,17 +1,5 @@
-"use client"
-
-import { NextPage, GetStaticProps } from "next";
-
-interface Props {
-    title: string
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-    return { props: { title: 'Finance' } }
-}
-
-const Page: NextPage<Props> = ({title}) => {
-    return <>{title}</>
+const Page = () => {
+    return <>Finance</>
 }
 
 export default Page;
