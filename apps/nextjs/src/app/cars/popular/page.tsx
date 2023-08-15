d8d0ff7926fa1bc6326c715bd0b0453f6e37ce15
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-15 19:15:20 +0545

    migrate top popular cars page

--- apps/nextjs/src/app/cars/popular/page.tsx
+++ apps/nextjs/src/app/cars/popular/page.tsx
@@ -1,17 +1,5 @@
-"use client"
-
-import { NextPage, GetStaticProps } from "next";
-
-interface Props {
-    title: string
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-    return { props: { title: 'Popular' } }
-}
-
-const Page: NextPage<Props> = ({title}) => {
-    return <>{title}</>
+const Page = () => {
+    return <>Popular</>
 }
 
 export default Page;
