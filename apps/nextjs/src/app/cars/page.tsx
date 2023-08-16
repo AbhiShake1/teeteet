18cdf905dab172f9fb58681460efdcbe78cae445
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-16 19:52:57 +0545

    use TCard in cars

--- apps/nextjs/src/app/cars/page.tsx
+++ apps/nextjs/src/app/cars/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import {prisma} from "@acme/db"
 import {oneDay} from "@acme/utils";
-import {Button} from "@acme/components";
+import {Button, TCard} from "@acme/components";
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
@@ -13,14 +13,17 @@ export const revalidate = oneDay
 
 const Page = async () => {
     const cars = await prisma.car.findMany()
-    return <div className='flex flex-col justify-center space-y-4'>
+    return <div className='grid grid-cols-1 gap-16 mx-auto lg:mx-0 md:grid-cols-3 px-16 pt-24'>
         {
-            cars.map(car => (
-                <Button key={car.id}>
-                    <Link href={`/cars/${car.id}`}>
-                        {JSON.stringify(car)}
+            cars.map(({id}) => (
+                <TCard key={id}>
+                    <Link
+                        href={`/cars/${id}`}
+                        target="_blank"
+                        className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                    >
                     </Link>
-                </Button>
+                </TCard>
             ))
         }
     </div>
