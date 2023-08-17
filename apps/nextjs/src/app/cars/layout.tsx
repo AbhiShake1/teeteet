a3c402fb1bef70a9e625d9f50cd2f653cb0780c0
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-17 19:57:07 +0545

    add some bottom padding to cars related pages

--- apps/nextjs/src/app/cars/layout.tsx
+++ apps/nextjs/src/app/cars/layout.tsx
@@ -1,7 +1,10 @@
 import {PropsWithChildren} from "react";
 
 export default function CarLayout({children}: PropsWithChildren) {
-    return <div className='grid grid-cols-1 gap-16 mx-auto lg:mx-0 md:grid-cols-3 px-16 pt-24'>
-        {children}
+    return <div className='flex flex-col'>
+        <div className='grid grid-cols-1 gap-16 mx-auto lg:mx-0 md:grid-cols-3 px-16 pt-24'>
+            {children}
+        </div>
+        <div className='h-16'/>
     </div>
 }
