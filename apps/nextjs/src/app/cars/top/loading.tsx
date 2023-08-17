05fc058628199348b7238d003a9a70a8ad5c3291
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-17 19:54:17 +0545

    add loader to top cars

--- apps/nextjs/src/app/cars/top/loading.tsx
+++ apps/nextjs/src/app/cars/top/loading.tsx
@@ -1,3 +1,12 @@
+import {Skeleton, TCard} from "@acme/components";
+import React from "react";
+
 export default function Loading() {
-    return <>loading...</>
+    return (
+        Array.from({length: 12}).map((_, idx) => (
+            <TCard key={idx}>
+                <Skeleton className='h-[30vh]'/>
+            </TCard>
+        ))
+    )
 }
