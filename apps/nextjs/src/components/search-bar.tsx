d9c2ff98706a8279f94a7d875ab7ffc8db9d9d0b
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-20 14:55:39 +0545

    add search params when typed in search bar

--- apps/nextjs/src/components/search-bar.tsx
+++ apps/nextjs/src/components/search-bar.tsx
@@ -2,9 +2,16 @@
 
 import {Input} from "@acme/components";
 import React from "react";
+import {usePathname, useRouter} from "next/navigation";
 
 export function SearchBar() {
+    const {replace} = useRouter()
+    const path = usePathname()
+
+    const setSearchParams = (search: string) => replace(`${path}?model=${search}`, {scroll: false})
+
     return (
-        <Input placeholder='Search...' className='max-w-xs bg-transparent backdrop-blur-3xl'/>
+        <Input placeholder='Search...' onChange={e => setSearchParams(e.target.value)}
+               className='max-w-xs bg-transparent backdrop-blur-3xl'/>
     )
 }
