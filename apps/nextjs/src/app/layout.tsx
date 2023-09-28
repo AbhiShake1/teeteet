88c2563ae508701f54a72a4fb549896c53329e5d
abhishake1-khalti <abhi9848038283@gmail.com>
2023-09-28 21:54:53 +0545

    fix clerk and remove trpc for clerk

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,15 +1,11 @@
-"use client"
-
 import "@acme/components/src/styles/globals.css";
-import {ClerkProvider, UserButton} from "@clerk/nextjs";
-import {api} from "../utils/trpc";
-import React, {useMemo} from "react";
-import {useTheme} from "next-themes"
-import {dark} from '@clerk/themes';
-import {TCard, TNavigationMenu, TThemeProvider} from "@acme/components";
-import {TContextMenu} from "@acme/components/src/core/TContextMenu";
-import {useRouter} from 'next/navigation';
+import {UserButton} from "@clerk/nextjs";
+import React from "react";
+import {TNavigationMenu, TThemeProvider} from "@acme/components";
 import {Lato} from 'next/font/google'
+import {ContextMenuProvider} from "../components/context-menu-provider";
+import {AuthProvider} from "../components/auth-provider";
+import {NavBar} from "../components/nav-bar";
 
 const font = Lato({
     subsets: ['latin'],
@@ -21,65 +17,20 @@ type AppProps = {
 }
 
 const RootLayout: React.FunctionComponent<AppProps> = ({children}) => {
-    const {back, forward, refresh} = useRouter()
-    const canGoBack = useMemo(() => {
-        if (typeof window == 'undefined') return false
-        // since 1 can be new tab
-        return window.history.length > 2
-    }, [])
-    const canGoForward = useMemo(() => {
-        if (typeof window == 'undefined') return false
-        return window.history.length > window.history.state?.index + 1
-    }, [])
-
     return (
         <html lang="en">
         <body className={font.className}>
-        <TContextMenu canGoBack={canGoBack} canGoForward={canGoForward} onBack={back} onReload={refresh}
-                      onForward={forward}>
-            <TThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-                <App>
+        <AuthProvider>
+            <ContextMenuProvider>
+                <TThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+                    <NavBar/>
                     {children}
-                </App>
-            </TThemeProvider>
-        </TContextMenu>
+                </TThemeProvider>
+            </ContextMenuProvider>
+        </AuthProvider>
         </body>
         </html>
     )
 }
 
-const App: React.FunctionComponent<AppProps> = ({children}) => {
-    const {theme} = useTheme()
-
-    return (
-        <>
-            <TNavBar/>
-            {children}
-        </>
-    )
-
-    return (
-        <ClerkProvider appearance={{
-            baseTheme: theme == 'dark' ? dark : undefined,
-        }}>
-            <TNavBar/>
-            {children}
-        </ClerkProvider>
-    )
-}
-
-const TNavBar = () => {
-    // const {isSignedIn} = useAuth()
-    const isSignedIn = false
-
-    return (
-        <nav
-            className='flex flex-row items-center justify-center py-4 w-full sticky top-0 z-[999] border-b backdrop-blur-[4px] shadow-lg shadow-black'>
-            <TNavigationMenu/>
-            <div className='fixed right-16'>{isSignedIn && <UserButton/>}</div>
-            {/*<TDarkModeToggle className='fixed right-36'/>*/}
-        </nav>
-    )
-}
-
-export default api.withTRPC(RootLayout);
+export default RootLayout;
---
+++ apps/nextjs/src/components/nav-bar.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import {TNavigationMenu} from "@acme/components";
+import React from "react";
+import {useAuth, UserButton} from "@clerk/nextjs"
+
+export function NavBar() {
+    const {isSignedIn} = useAuth()
+
+    return <nav
+        className='flex flex-row items-center justify-center py-4 w-full sticky top-0 z-[999] border-b backdrop-blur-[4px] shadow-lg shadow-black'>
+        <TNavigationMenu/>
+        <div className='fixed right-16'>{isSignedIn && <UserButton/>}</div>
+        {/*<TDarkModeToggle className='fixed right-36'/>*/}
+    </nav>
+}
